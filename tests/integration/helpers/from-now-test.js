c46bbb7535758eaa27ce8cf9bde670d0ae717d61
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2017-12-03 23:34:25 +0100

    Tests

--- tests/integration/helpers/from-now-test.js
+++ tests/integration/helpers/from-now-test.js
@@ -1,17 +1,20 @@
-
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+import { moduleForComponent, test } from 'ember-qunit'
+import hbs from 'htmlbars-inline-precompile'
 
 moduleForComponent('from-now', 'helper:from-now', {
   integration: true
-});
+})
 
 // Replace this with your real tests.
 test('it renders', function(assert) {
-  this.set('inputValue', '1234');
+  this.set('inputValue', '1234')
 
-  this.render(hbs`{{from-now inputValue}}`);
+  this.render(hbs`{{from-now inputValue}}`)
 
-  assert.equal(this.$().text().trim(), '1234');
-});
-
+  assert.equal(
+    this.$()
+      .text()
+      .trim(),
+    '1234'
+  )
+})
